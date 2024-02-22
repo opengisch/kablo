c3e6f680b1187c0581814464ae86b1b765aaae68
Damiano Lombardi <damiano@opengis.ch>
2024-02-22 10:59:36 +0100

    More classes

--- kablo/apps/network/models.py
+++ kablo/apps/network/models.py
@@ -5,7 +5,7 @@ class NetworkNode(models.Model):
     geom = models.PointField(srid=2056)
 
 
-class NetworkSegment(models.Model):
+class TrackSection(models.Model):
     geom = models.LineStringField(srid=2056)
     network_node_start = models.ForeignKey(
         NetworkNode, related_name="network_node_start", on_delete=models.CASCADE
@@ -16,22 +16,65 @@ class NetworkSegment(models.Model):
 
 
 class Track(models.Model):
-    networkSegments = models.ManyToManyField(NetworkSegment)
+    networkSegments = models.ManyToManyField(
+        TrackSection, through="TrackNetworkSegment"
+    )
+
+    geom = models.GeneratedField(
+        expression=F("side") * F("side"),
+        output_field=models.BigIntegerField(),
+        db_persist=True,
+    )
+
+    def save(self, **kwargs):
+        if not self.networkSegments:
+            network_segment = TrackSection.objects.create()
+            network_segment.geom = self.geom
+            network_segment.save()
+
+        super().save(**kwargs)
+
+        self.networkSegments.add(network_segment)
+
+
+class TrackTrackSection(models.Model):
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+    network_node = models.ForeignKey(NetworkNode, on_delete=models.CASCADE)
+    index = models.IntegerField()
+
+    def save(self, **kwargs):
+        if not self.index:
+            self.index = len(self.track.networkSegments)
+
+        super().save(**kwargs)
 
 
 class Tube(models.Model):
-    networkSegments = models.ManyToManyField(NetworkSegment)
+    networkSegments = models.ManyToManyField(TrackSection)
 
 
 class Station(models.Model):
-    position = models.PointField(srid=2056)
+    geom = models.PointField(srid=2056)
 
 
 class Cable(models.Model):
-    tubes = models.ManyToManyField(Tube)
     station_start = models.ForeignKey(
         Station, related_name="station_start", on_delete=models.CASCADE
     )
     station_end = models.ForeignKey(
         Station, related_name="station_end", on_delete=models.CASCADE
     )
+
+    tubes = models.ManyToManyField(Tube)
+
+
+class Node(models.Model):
+    pass
+
+
+class Switch(models.Model):
+    pass
+
+
+class Clamp(models.Model):
+    pass
