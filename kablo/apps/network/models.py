43e73c873c2b612e42e95198e90497b884098f5b
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-22 13:39:34 +0100

    aggregate

--- kablo/apps/network/models.py
+++ kablo/apps/network/models.py
@@ -1,50 +1,69 @@
+import uuid
+
 from django.contrib.gis.db import models
+from django.db import transaction
+
+from kablo.apps.core.geometry import MergeLines
 
 
 class NetworkNode(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     geom = models.PointField(srid=2056)
 
 
 class TrackSection(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     geom = models.LineStringField(srid=2056)
     network_node_start = models.ForeignKey(
-        NetworkNode, related_name="network_node_start", on_delete=models.CASCADE
+        NetworkNode,
+        null=True,
+        blank=True,
+        related_name="network_node_start",
+        on_delete=models.SET_NULL,
     )
     network_node_end = models.ForeignKey(
-        NetworkNode, related_name="network_node_end", on_delete=models.CASCADE
+        NetworkNode,
+        null=True,
+        blank=True,
+        related_name="network_node_end",
+        on_delete=models.SET_NULL,
     )
 
 
 class Track(models.Model):
-    networkSegments = models.ManyToManyField(
-        TrackSection, through="TrackNetworkSegment"
-    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    geom = models.LineStringField(srid=2056)
 
-    geom = models.GeneratedField(
-        expression=F("side") * F("side"),
-        output_field=models.BigIntegerField(),
-        db_persist=True,
-    )
+    track_sections = models.ManyToManyField(TrackSection, through="TrackTrackSection")
+
+    def compute_geom(self):
+        return (
+            self.track_sections.all()
+            .order_by("index")
+            .aggregate(geom=MergeLines("geom"))
+            .values_list("geom")
+        )
 
     def save(self, **kwargs):
-        if not self.networkSegments:
-            network_segment = TrackSection.objects.create()
-            network_segment.geom = self.geom
-            network_segment.save()
 
-        super().save(**kwargs)
-
-        self.networkSegments.add(network_segment)
+        with transaction.atomic():
+            if not self.track_sections:
+                track_section = TrackSection.objects.create()
+                track_section.geom = self.geom
+                track_section.save()
+                self.track_sections.add(track_section)
+            super().save(**kwargs)
 
 
 class TrackTrackSection(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     track = models.ForeignKey(Track, on_delete=models.CASCADE)
-    network_node = models.ForeignKey(NetworkNode, on_delete=models.CASCADE)
+    track_section = models.ForeignKey(TrackSection, on_delete=models.CASCADE)
     index = models.IntegerField()
 
     def save(self, **kwargs):
         if not self.index:
-            self.index = len(self.track.networkSegments)
+            self.index = len(self.track.track_section)
 
         super().save(**kwargs)
 
