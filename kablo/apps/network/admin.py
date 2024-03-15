300da7b8ae3e2b85ed78f8182b93d631c7e65ed9
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-03-15 16:09:50 +0100

    add inlines to admin
    mostly taken from #18 with:
    * fix Track / TrackSection ambiguity
    * add Track and TrackSection relation

--- kablo/apps/network/admin.py
+++ kablo/apps/network/admin.py
@@ -8,16 +8,52 @@ from .models import (
     Terminal,
     Track,
     TrackSection,
-    TrackTrackSection,
     Tube,
 )
 
-admin.site.register(NetworkNode)
-admin.site.register(TrackSection)
-admin.site.register(Track)
-admin.site.register(TrackTrackSection)
-admin.site.register(Tube)
-admin.site.register(Station)
+
+# Basic demo showing cascaded relationships: track sections => tubes => cables
+class CableInline(admin.TabularInline):
+    model = Cable.tubes.through
+
+
+class TubeInline(admin.TabularInline):
+    model = Tube.track_sections.through
+    inlines = [
+        CableInline,
+    ]
+
+
+class TrackSectionInline(admin.TabularInline):
+    model = Track.track_sections.through
+
+
+class TubeAdmin(admin.ModelAdmin):
+    model = Tube
+    inlines = [
+        CableInline,
+    ]
+
+
+class TrackAdmin(admin.ModelAdmin):
+    model = Track
+    inlines = [
+        TrackSectionInline,
+    ]
+
+
+class TrackSectionAdmin(admin.ModelAdmin):
+    model = TrackSection
+    inlines = [
+        TubeInline,
+    ]
+
+
 admin.site.register(Cable)
+admin.site.register(NetworkNode)
+admin.site.register(Station)
 admin.site.register(Switch)
 admin.site.register(Terminal)
+admin.site.register(Track, TrackAdmin)
+admin.site.register(TrackSection, TrackSectionAdmin)
+admin.site.register(Tube, TubeAdmin)
