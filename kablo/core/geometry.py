6e16ae50088191042b187b6942c49c92914185a4
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-03-20 08:50:25 +0100

    drop unused methods

--- kablo/core/geometry.py
+++ kablo/core/geometry.py
@@ -1,14 +1,6 @@
 from django.contrib.gis.db import models
 from django.contrib.gis.db.models.functions import GeoFunc
 
-# from django.db.models import Aggregate
-
-
-# class MergeLines(Aggregate):
-#    name = "joined_geometries"
-#    template = "ST_SetSRID(ST_Expand(ST_Extent(%(expressions)s), 1), 2056)"
-#    allow_distinct = False
-
 
 class Intersects(GeoFunc):
     function = "ST_Intersects"
@@ -20,8 +12,3 @@ class SplitLine(GeoFunc):
     function = "ST_Split"
     geom_param_pos = (0, 1)
     output_field = models.MultiLineStringField()
-
-
-class Dump(GeoFunc):
-    function = "ST_Dump"
-    geom_param_pos = [0]
