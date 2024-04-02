d0863ec3948b89066d4b0bdfec7491f4cf657e70
monodo <olivier.monod@yverdon-les-bains.ch>
2024-04-02 10:02:51 +0200

    add value lists

---
+++ kablo/valuelist/models.py
@@ -0,0 +1,35 @@
+import uuid
+
+from django.db import models
+from django_oapif.decorators import register_oapif_viewset
+
+
+class AbstractValueList(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    original_id = models.PositiveIntegerField(null=True)
+    original_uuid = models.UUIDField(null=True, editable=True)
+    code = models.PositiveIntegerField(null=True)
+    name_fr = models.CharField(max_length=64, blank=True)
+    index = models.PositiveIntegerField(null=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"{self.original_id}-{self.name_fr}"
+
+
+@register_oapif_viewset(geom_field=None)
+class StatusType(AbstractValueList):
+    pass
+
+
+@register_oapif_viewset(geom_field=None)
+class TubeCableProtectionType(AbstractValueList):
+    pass
+
+
+@register_oapif_viewset(geom_field=None)
+class CableTensionType(AbstractValueList):
+    pass
