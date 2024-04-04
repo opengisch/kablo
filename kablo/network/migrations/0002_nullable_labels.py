56a5d91955cf753f46d37f8ce268875da54695e3
monodo <olivier.monod@yverdon-les-bains.ch>
2024-04-04 14:52:22 +0200

    lint

--- kablo/network/migrations/0002_nullable_labels.py
+++ kablo/network/migrations/0002_nullable_labels.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('network', '0001_initial'),
+        ("network", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='station',
-            name='label',
+            model_name="station",
+            name="label",
             field=models.CharField(blank=True, max_length=64, null=True),
         ),
     ]
